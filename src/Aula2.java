f78aa68bbc89648601e64f56621e9956d41d8fc3
Marieli <marieliasantos@hotmail.com>
2022-09-10 20:33:40 -0300

    True or false.

---
+++ src/Aula2.java
@@ -0,0 +1,29 @@
+public class Aula2 {
+    public static void main(String[] args) {
+
+       boolean fimDeSemana = false;
+       boolean fazendoSol = false;
+       boolean vamosAPraia = fimDeSemana && fazendoSol;
+        System.out.println(vamosAPraia);
+
+        // Tabela verdade
+        // Operador && (AND)
+        // true && true = true
+        // true && false = false
+        // false && true = false
+        // false && false = false
+
+        // Operador || (OR) pipe pie
+        // true || true = true
+        // true || false = true
+        // false || true = true
+        // false || false = false
+
+        System.out.println(vamosAPraia);
+
+        String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+        System.out.println(mensagem);
+    }
+
+
+}
